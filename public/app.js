aa1296513cf76dcb289c77b7b4362b75b98383fb
jtmoldrup <jtmoldrup@ksu.edu>
2017-03-30 22:32:14 -0500

    now shows individual data

--- public/app.js
+++ public/app.js
@@ -1,41 +1,75 @@
+function loadTeam(teamIdurl){
+    $.get(teamIdurl, function(team, status){
+        if(status == "success"){
+            var dataSection = $('#data-display');
+            dataSection.empty();
+            var teamImageDiv = $('<div class="team-logo">')
+            var imageTag = $('<img class="logo-large">')
+                .attr('src', team.image)
+                .appendTo(teamImageDiv);
+            teamImageDiv.appendTo(dataSection);
+
+            var teamDataDiv = $('<div class="team-data">')
+            var ul = $('<ul>');
+            var teamNameLi = $('<li>')
+                .text('Team Name: ' + team.name)
+                .appendTo(ul);
+            var teamCoachLi = $('<li>')
+                .text('Coach: ' + team.coach)
+                .appendTo(ul);
+            var teamNameLi = $('<li>')
+                .text('Record: ' + team.record)
+                .appendTo(ul);
+            var teamNameLi = $('<li>')
+                .text('Description: ' + team.description)
+                .appendTo(ul);   
+            ul.appendTo(teamDataDiv);
+            teamDataDiv.appendTo(dataSection);
+        }
+    });
+}
+
+
+
 
 function loadIndex(){
   $.get('/teams', function(teams, status){
     if(status == "success"){
       teams.forEach(function(team){
-        var link = $('<a>')
-        //   .text(team.image)
-          .attr('href', '/teams/' + team.id)
-          .on('click', function(){
-            e.preventDefault();
-            loadTeam('/teams/' + team.id);
-          }).appendTo('#data-display');
+        // var link = $('<a>')
+        //     .text(team.image)
+        //     .attr('href', '/teams/' + team.id)
+            
           var imageTag = $('<img class="clipboard-image">');
           imageTag.attr('src', team.image);
-          imageTag.appendTo(link);
+          imageTag.on('click', function(){
+            loadTeam('/teams/' + team.id);
+          }).appendTo('#data-display');
       });
+      $('<br>').appendTo('#data-display');
+      $('<br>').appendTo('#data-display');
       $('<button>').text('Add Team').on('click', function(){
           // adds form to page
           $('#data-display').empty();
           $('#data-display').load('/public/team-form.html', function(){
                 $('form').on('submit',function(event){
-                        event.preventDefault();
-                        $('#data-display').empty();
-                        var data = new FormData($('#team-form')[0]);
-                        console.log('Form Data:');
-                        console.log('name:' + data.name);
-                        console.log('name:' + data.coach);
-                        console.log('name:' + data.record);
-                        console.log('name:' + data.description);
-                        console.log('name:' + data.imagePath);
-                        console.log('name:' + data.image);
+                    event.preventDefault();
+                    $('#data-display').empty();
+                    var data = new FormData($('#team-form')[0]);
+                    console.log('Form Data:');
+                    console.log('name:' + data.name);
+                    console.log('name:' + data.coach);
+                    console.log('name:' + data.record);
+                    console.log('name:' + data.description);
+                    console.log('name:' + data.imagePath);
+                    console.log('name:' + data.image);
 
-                        $.post({
-                            url: '/teams',
-                            data: data,
-                            contentType: 'multipart/form-data',
-                            processData: false
-                        });
+                    $.post({
+                        url: '/teams',
+                        data: data,
+                        contentType: 'multipart/form-data',
+                        processData: false
+                    });
                 });
           });
       }).appendTo('#data-display');
@@ -45,45 +79,45 @@ function loadIndex(){
   });
 }
 
-function loadTeam(url){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send(null);
+// function loadTeam(url){
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     xhr.send(null);
 
-    xhr.onreadystatechange = function() {
-      var DONE = 4; // readyState 4 means the request is done.
-      var OK = 200; // status 200 is a successful return.
-      if (xhr.readyState === DONE) {
-          if (xhr.status === OK) {
-              console.log(xhr.responseText); // 'This is the returned text.'
-              var bodyContent = document.body.getElementById('data-display');
-              bodyContent.innerHTML = "";
-              var team = JSON.parse(xhr.responseText);
-              var wrapper = document.createElement('div')
-              var name = document.createElement('h1');
+//     xhr.onreadystatechange = function() {
+//       var DONE = 4; // readyState 4 means the request is done.
+//       var OK = 200; // status 200 is a successful return.
+//       if (xhr.readyState === DONE) {
+//           if (xhr.status === OK) {
+//               console.log(xhr.responseText); // 'This is the returned text.'
+//               var bodyContent = document.body.getElementById('data-display');
+//               bodyContent.innerHTML = "";
+//               var team = JSON.parse(xhr.responseText);
+//               var wrapper = document.createElement('div')
+//               var name = document.createElement('h1');
               
-              var image = document.createElement('img');
-              var coach = document.createElement('h4');
-              var record = document.createElement('h4');
-              var description = document.createElement('h4');
-              name.innerHTML = team.name;
-              image.src = team.image;
-              coach.innerHTML = team.name;
-              record.innerHTML = team.name;
-              description.innerHTML = team.name;
-              wrapper.appendChild(name);
-              wrapper.appendChild(image);
-              wrapper.appendChild(record);
-              wrapper.appendChild(coach);
-              wrapper.appendChild(description);
-              bodyContent.appendChild(wrapper);
-          } else {
-              console.log('Error: ' + xhr.status); // An error occurred during the request.
-          }
-        }
-    }
+//               var image = document.createElement('img');
+//               var coach = document.createElement('h4');
+//               var record = document.createElement('h4');
+//               var description = document.createElement('h4');
+//               name.innerHTML = team.name;
+//               image.src = team.image;
+//               coach.innerHTML = team.name;
+//               record.innerHTML = team.name;
+//               description.innerHTML = team.name;
+//               wrapper.appendChild(name);
+//               wrapper.appendChild(image);
+//               wrapper.appendChild(record);
+//               wrapper.appendChild(coach);
+//               wrapper.appendChild(description);
+//               bodyContent.appendChild(wrapper);
+//           } else {
+//               console.log('Error: ' + xhr.status); // An error occurred during the request.
+//           }
+//         }
+//     }
 
-}
+// }
 
 function uploadTeam(){
     var xhr = new XMLHttpRequest();
